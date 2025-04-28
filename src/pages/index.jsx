9fe07feaa9dcbe7094b47b3958c4fd4abb6b424c
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-28 15:42:52 +0200

    vyřešeno

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,14 @@
 import { render } from "@czechitas/render";
+import { Episode } from "../components/Episode/Episode";
+import { episodes } from "./data";
 import "../global.css";
 import "./index.css";
-
-const podcast1 = {
-  num: "126",
-  title: "Robot, který snědl koblihu",
-  guest: "Radovan Siwek",
-};
+import "../components/Episode/Episode.css"
 
 document.querySelector("#root").innerHTML = render(
   <>
     <div className="episodes-list">
-      <div className="episode">
-        <div className="episode__num">127</div>
-        <div className="episode__body">
-          <div className="episode__title">Hledání plyšového Yettiho</div>
-          <div className="episode__guest">host: Vojtěch Ryba</div>
-        </div>
-      </div>
-
-      <div className="episode">
-        <div className="episode__num">128</div>
-        <div className="episode__body">
-          <div className="episode__title">Moderní hrachová kaše</div>
-          <div className="episode__guest">host: Kamila Štancová</div>
-        </div>
-      </div>
+      {episodes.map(({num, title, guest}) => <Episode key={num} num={num} title={title} guest={guest}/>)}
     </div>
   </>
 );
